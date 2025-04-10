34aa4390f266b8423a38a18953ca4881d9a6c3c8
Sudharshan V <sudarshan61kv@gmail.com>
2025-04-10 10:27:48 +0530

    changes

--- .config/sketchybar/items/calendar.sh
+++ .config/sketchybar/items/calendar.sh
@@ -2,5 +2,6 @@
 
 sketchybar --add item calendar right \
            --set calendar icon=📅 \
-                          update_freq=30 \
+                          update_freq=10 \
+                          background.color=0x44ffffff \
                           script="$PLUGIN_DIR/calendar.sh"
--- .config/sketchybar/items/volume.sh
+++ .config/sketchybar/items/volume.sh
@@ -2,4 +2,5 @@
 
 sketchybar --add item volume right \
            --set volume script="$PLUGIN_DIR/volume.sh" \
+                 background.color=0x44ffffff \
            --subscribe volume volume_change \
