b70489555dc218b624406d6dffbf5af2783b3d83
Sudharshan V <sudarshan61kv@gmail.com>
2025-10-10 13:08:26 +0530

    changes

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -44,23 +44,6 @@ return {
 	"petertriho/nvim-scrollbar",
 	"zyedidia/vim-snake",
 	"sharkdp/fd",
-	{
-		"linux-cultist/venv-selector.nvim",
-		dependencies = {
-			"neovim/nvim-lspconfig",
-			"mfussenegger/nvim-dap",
-			"mfussenegger/nvim-dap-python", --optional
-			{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-		},
-		lazy = false,
-		branch = "regexp", -- This is the regexp branch, use this for the new version
-		config = function()
-			require("venv-selector").setup()
-		end,
-		keys = {
-			{ ",v", "<cmd>VenvSelect<cr>" },
-		},
-	},
 
 	{
 		"HiPhish/nvim-ts-rainbow2",
