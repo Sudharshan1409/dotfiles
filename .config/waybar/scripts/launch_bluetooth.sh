4413899f690d7120934fb9d92dde3b8e1c3eb587
Sudharshan V <sudarshan61kv@gmail.com>
2025-11-06 01:12:11 +0530

    changes

--- .config/waybar/scripts/launch_bluetooth.sh
+++ .config/waybar/scripts/launch_bluetooth.sh
@@ -4,7 +4,6 @@ if [ -f /etc/os-release ]; then
     . /etc/os-release
     if [[ "$ID" == "arch" || "$ID_LIKE" == "arch" ]]; then
         blueberry &
-        bzmenu -l custom --launcher-command ~/.config/rofi/rofi-bluetooth.sh &
     else
         # Default fallback
         bzmenu -l custom --launcher-command ~/.config/rofi/rofi-bluetooth.sh &
